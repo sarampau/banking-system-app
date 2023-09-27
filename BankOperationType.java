03751b1289813064ba27b66059ea226c3a28a8b4
Manny <mannysth@yahoo.com>
2023-09-27 12:26:23 -0700

    added simple tests to app

--- BankOperationType.java
+++ BankOperationType.java
@@ -1,5 +1,9 @@
 public enum BankOperationType {
-    NEW_ACCOUNT(1), DEPOSIT(2),  WITHDRAWAL(3), CHECK(4), UNKNOWN(-1);
+    NEW_ACCOUNT(1),
+    DEPOSIT(2),
+    WITHDRAWAL(3),
+    CHECK(4),
+    UNKNOWN(-1);
 
     private final int value;
 
